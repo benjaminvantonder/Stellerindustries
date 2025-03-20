bd271f171bb974aff1419f2812404fe3c1044bbf
jeproch <jprevenant@gmail.com>
2025-03-21 00:00:39 +0200

    various changes

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //write code to change pages here
+
+function goToPageServices() {
+  window.location.href = "./services-page/services.html";
+}
+
+function goToPageContact() {
+  window.location.href = "./contact-page/contact.html";
+}
+
+function goToPageAboutUs() {
+  window.location.href = "./about-us-page/about-us.html";
+}
