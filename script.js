19d9197b94c9a0f6b0afdf46fdafe17f471c19d0
jsmaniac0 <vissievanafrika@gmail.com>
2025-03-30 17:11:15 +0200

    fsdfds

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
 //write code to change pages here
+/*
+1.	Home (Landing Page)
+2.	About Us
+3.	Services (Dropdown with subpages)
+4.	Portfolio
+5.	Testimonials
+6.	Blog (Optional for updates, tips, and news)
+7.	Contact Us
+
+*/
+
+function goToPageHome() {
+  window.location.href = "#";
+}
+
+function goToPageAboutUs() {
+  window.location.href = "./about-us-page/about-us.html";
+}
 
 function goToPageServices() {
   window.location.href = "./services-page/services.html";
 }
 
-function goToPageContact() {
-  window.location.href = "./contact-page/contact.html";
+function goToPagePortfolio() {
+  window.location.href = "./services-page/services.html";
 }
 
-function goToPageAboutUs() {
-  window.location.href = "./about-us-page/about-us.html";
+function goToPageTestimonials() {
+  window.location.href = "./services-page/services.html";
+}
+
+function goToPageBlog() {
+  window.location.href = "./services-page/services.html";
+}
+
+function goToPageContact() {
+  window.location.href = "./contact-page/contact.html";
 }
