d0cce4fec06f67244c9fc86b63ba723ec25d0acd
jsmaniac0 <vissievanafrika@gmail.com>
2025-03-30 21:06:10 +0200

    all navigation except for services

---
+++ blog-page/blog.js
@@ -0,0 +1,42 @@
+function goToPageHome() {
+  window.location.href = "../index.html";
+}
+
+function goToPageAboutUs() {
+  window.location.href = "../about-us-page/about-us.html";
+}
+
+function goToPageServicesSound() {
+  window.location.href = "../services-page/services-sound-and-lighting.html"; //services-page\services-sound-and-lighting.html
+}
+
+function goToPageServicesPhotography() {
+  window.location.href =
+    "../services-page/services-photography-and-videography.html";
+}
+
+function goToPageServicesInstallations() {
+  window.location.href =
+    "../services-page/services-installations-and-consulting.html";
+}
+
+function goToPageServicesSupport() {
+  window.location.href =
+    "../services-page/services-event-support-and-rentals.html";
+}
+
+function goToPagePortfolio() {
+  window.location.href = "../portfolio-page/portfolio.html";
+}
+
+function goToPageTestimonials() {
+  window.location.href = "#";
+}
+
+function goToPageBlog() {
+  window.location.href = "#";
+}
+
+function goToPageContact() {
+  window.location.href = "../contact-page/contact.html";
+}
